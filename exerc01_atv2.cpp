d3d6040c04a84209c64675aca3edcbe969c5c6b6
eduardomontb <eduardomontb@gmail.com>
2025-09-03 19:05:42 -0400

    Update exerc01_atv2.cpp

--- exerc01_atv2.cpp
+++ exerc01_atv2.cpp
@@ -1,13 +1,15 @@
-//Faça um programa que receba quatro notas de um aluno, calcule e imprima a média aritmética
-//das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de
-//reprovado para média inferior a 7.0.
+//FaÃ§a um programa que receba quatro notas de um aluno, calcule e imprima a mÃ©dia aritmÃ©tica
+//das notas e a mensagem de aprovado para mÃ©dia superior ou igual a 7.0 ou a mensagem de
+//reprovado para mÃ©dia inferior a 7.0.
 
 #include <stdio.h>
 
 int main(){
-	
+
+	//declaraÃ§Ã£o de variavel
 	float n1, n2, n3, n4, media;
-	
+
+	//entrada de dados
 	printf("Digite o valor da primeira nota = ");
 	scanf("%f", &n1);
 	
@@ -19,9 +21,11 @@ int main(){
 	
 	printf("Digite o valor da quarta a nota = ");
 	scanf("%f", &n4);
-	
+
+	//processamento de dados
 	media = (n1 + n2 + n3 + n4) / 4;
-	
+
+	// Estrutura condicional 
 	if (media >= 7){
 	   printf("\n\n\nAluno aprovado!!\n");
 }	else{
