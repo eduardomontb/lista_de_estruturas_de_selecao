906d93f30ab1cb04cd6b7e77c65d0abfd880089b
eduardomontb <eduardomontb@gmail.com>
2025-09-03 21:17:46 -0400

    Update exerc07_atv2.cpp

--- exerc07_atv2.cpp
+++ exerc07_atv2.cpp
@@ -1,22 +1,25 @@
-//Faça um programa que receba a idade de um nadador e imprima a sua categoria seguindo as regras:
+//FaÃ§a um programa que receba a idade de um nadador e imprima a sua categoria seguindo as regras:
 //categoria idade
-//infantil A 5 – 7 anos
-//infantil B 8 – 10 anos
-//juvenil A 11 – 13 anos
-//juvenil B 14 – 17 anos
-//sênior maiores de 18 anos
+//infantil A 5 â€“ 7 anos
+//infantil B 8 â€“ 10 anos
+//juvenil A 11 â€“ 13 anos
+//juvenil B 14 â€“ 17 anos
+//sÃªnior maiores de 18 anos
 
 #include <stdio.h>
 
 int main (){
-	
+
+	//declaraÃ§Ã£o de varriaveis
 	int idade;
-	 
+
+	//entrada de dados
 	printf("Digite sua idade = ");
 	scanf("%i", &idade);
 	
-	
-if (idade >= 18) {
+//processamento de dados
+//Estrutura condicional	
+ if (idade >= 18) {
         printf("Categoria: Senior\n");
     } else if (idade >= 14) {
         printf("Categoria: Juvenil B\n");
@@ -27,7 +30,7 @@ if (idade >= 18) {
     } else if (idade >= 5) {
         printf("Categoria: Infantil A\n");
     } else {
-        printf("Não possui idade suficiente.\n");
+        printf("NÃ£o possui idade suficiente.\n");
     }
 
 	 return 0;  
