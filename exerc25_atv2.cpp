20f38b2512af0d2ff7a1c32aba9504f31066c13b
eduardomontb <eduardomontb@gmail.com>
2025-09-03 22:53:07 -0400

    Update exerc25_atv2.cpp

--- exerc25_atv2.cpp
+++ exerc25_atv2.cpp
@@ -1,28 +1,31 @@
-//Escreva um programa que calcule o que deve ser pago por um produto, considerando o preÁo normal de etiqueta
-//e a escolha da condiÁ„o de pagamento. Utilize os cÛdigos da tabela a seguir para ler qual a condiÁ„o de pagamento escolhida e efetuar o c·lculo adequado:
+//Escreva um programa que calcule o que deve ser pago por um produto, considerando o pre√ßo normal de etiqueta
+//e a escolha da condi√ß√£o de pagamento. Utilize os c√≥digos da tabela a seguir para ler qual a condi√ß√£o de pagamento escolhida e efetuar o c√°lculo adequado:
 
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
 	
-	//declaraÁ„o de variaveis
+	//declara√ß√£o de variaveis
 	float preco, valor_final;
 	int codigo;
 	
 	//etrada de dados
 	printf("Digite o valor do produto = ");
 	scanf("%f", &preco);
-	
+
+	// menu de op√ß√µes de pagamento
 	printf("\ncondicao de pagamento\n");
 	printf("1 - a vista em dinheiro ou cheque, recebe 10 porcento de desconto.\n");
-	printf("2 - a vista no cart„o de crÈdito, recebe 5 porcento de desconto.\n");
-	printf("3 - Em 2 vezes, preÁo normal de etiqueta sem juros.\n");
-	printf("4 - Em 3 vezes, preÁo normal de etiqueta mais juros de 10%.\n");
-	
+	printf("2 - a vista no cart√£o de cr√©dito, recebe 5 porcento de desconto.\n");
+	printf("3 - Em 2 vezes, pre√ßo normal de etiqueta sem juros.\n");
+	printf("4 - Em 3 vezes, pre√ßo normal de etiqueta mais juros de 10%.\n");
+
+	//leitura de op√ß√£o escolhida
 	printf("escolha a condicao de pagamento = ");
 	scanf("%i", &codigo);
-	
+
+	//estrutura condicional para verificar forma de pagamneto
 	if (codigo == 1){
 		
 		printf("\nCondicao de pagamneto escolhida : a vista em dinheiro ou cheque, recebe 10 porcento de desconto.\n ");
@@ -30,23 +33,23 @@ int main(){
 		
 	}else if (codigo == 2){
 		
-		printf("\nCondicao de pagamneto escolhida :  a vista no cart„o de crÈdito, recebe 5 porcento de desconto.\n ");
+		printf("\nCondicao de pagamneto escolhida :  a vista no cart√£o de cr√©dito, recebe 5 porcento de desconto.\n ");
 		valor_final = preco * 0.95;
 		
 	}else if (codigo == 3){
 		
-		printf("\nCondicao de pagamneto escolhida : Em 2 vezes, preÁo normal de etiqueta sem juros.\n ");
+		printf("\nCondicao de pagamneto escolhida : Em 2 vezes, pre√ßo normal de etiqueta sem juros.\n ");
 		valor_final = preco;
 		
 	}else if (codigo == 4){
 		
-		printf("\nCondicao de pagamneto escolhida : Em 3 vezes, preÁo normal de etiqueta mais juros de 10%.\n ");
+		printf("\nCondicao de pagamneto escolhida : Em 3 vezes, pre√ßo normal de etiqueta mais juros de 10%.\n ");
 		valor_final = preco * 1.10;
 	}else{
 		printf("\nCodigo invalido\n");
 	}
 	
-	
+	//saidad de  dados
 	printf("O preco final do produto = %2.f", valor_final);
 	
 	return 0;
