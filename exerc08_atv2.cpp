0c26063ad56893f5ed3196e0a16e80f7da5162ac
eduardomontb <eduardomontb@gmail.com>
2025-09-03 22:30:44 -0400

    Update exerc08_atv2.cpp

--- exerc08_atv2.cpp
+++ exerc08_atv2.cpp
@@ -1,15 +1,15 @@
-//No curso de Desenvolvimento de Software, a nota final do estudante é calculada a partir de 
-//3 notas atribuídas respectivamente a um trabalho de laboratório, a uma avaliação semestral
-//e a um exame final. As notas variam de 0 a 10 e a nota final é média ponderada das 3 notas
+//No curso de Desenvolvimento de Software, a nota final do estudante Ã© calculada a partir de 
+//3 notas atribuÃ­das respectivamente a um trabalho de laboratÃ³rio, a uma avaliaÃ§Ã£o semestral
+//e a um exame final. As notas variam de 0 a 10 e a nota final Ã© mÃ©dia ponderada das 3 notas
 //mencionadas. A tabela a seguir fornece os pesos das notas:
-//Laboratório - peso 2
+//LaboratÃ³rio - peso 2
 //Av. Semestral - peso 3
 //Exame final - peso 5
  
-//Faça um programa que receba as 3 notas do estudante, calcule e imprima a
-//média final e o conceito desse estudante.
+//FaÃ§a um programa que receba as 3 notas do estudante, calcule e imprima a
+//mÃ©dia final e o conceito desse estudante.
 //O conceito segue a tabela abaixo:
-//média final conceito
+//mÃ©dia final conceito
 //8.0 |__| 10.0 A
 //7.0 |__ 8.0 B
 //6.0 |__ 7.0 C
@@ -18,10 +18,13 @@
 
 #include <stdio.h>
 
+
 int main(){
-	
+
+	//declaraÃ§Ã£o de varriaveis
 	float n1, n2, n3, media_ponderada;
-	
+
+	//entrada de dados
 	printf("Digite o valor da primeira nota = ");
 	scanf("%f", &n1);
 	
@@ -30,11 +33,15 @@ int main(){
 	
 	printf("Digite o valor da terceira nota = ");
 	scanf("%f", &n3);
-	
+
+	//processamento de dados
+	//calculo da media ponderada
 	media_ponderada = (n1 * 2 + n2 * 3 + n3 * 5) / 10;
-	
+
+	//mostra a media ponderada
 	printf("\n\n\nA media ponderada de suas notas e = %.2f\n", media_ponderada);
-	
+
+	//estrutura condicional
     if (media_ponderada >= 8 && media_ponderada <= 10){
         printf("Conceito : A\n");
 	}else if (media_ponderada >= 7){
