c799a1943bc5ebfdc0a93b5143b9eb39b1625058
eduardomontb <eduardomontb@gmail.com>
2025-09-03 22:48:48 -0400

    Update exerc12_atv2.cpp

--- exerc12_atv2.cpp
+++ exerc12_atv2.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	
+
+	//declaração de variantes
 	float n1, n2, n3, media_aritmetica;
-	
+
+	//entrada de dados
 	printf("digite o valor das tres notas\n");
 	scanf("%f %f %f", &n1, &n2, &n3);
-	
+
+	//processamento de dados
+	//calculo da media aritmetica
 	media_aritmetica = (n1 + n2 + n3) / 3;
-	
+
+	//estrutura condicional
 	if(media_aritmetica >= 7){
 		printf("Aluno aprovado");
 	}else if (media_aritmetica >= 5 ){
